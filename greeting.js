6013c0a369bc8525669641799c654423a393c1af
Roman Jaeger <roman.jaeger@gmx.net>
2019-11-18 20:26:15 +0100

    add greeting.js

---
+++ greeting.js
@@ -0,0 +1,23 @@
+var d = new Date();
+var time = d.getHours();
+
+if (time < 12) {
+    var greeting = "Guten Morgen";
+    var emoji = "🌞";
+}
+if (time > 12 && time <18) {
+    var greeting = "Hallo";
+    var emoji = "☀";
+}
+if (time > 18) {
+    var greeting = "Guten Abend";
+    var emoji = "🌛";
+}
+
+var greetingSelector = document.querySelector('#greeting');
+
+if (greetingSelector) {
+	greetingSelector.innerHTML = greeting + ' ' + we.util.ui.name(we.authMgr.getUser()) + ' Schön, dass Du da bist! ' + emoji;
+} else {
+    console.log("This didn't work. Try again");
+}
