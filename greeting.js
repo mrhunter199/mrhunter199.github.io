ab1606aa0b602e7f8ca01efdd3d9cba479b1735a
Roman Jaeger <roman.jaeger@gmx.net>
2019-11-18 20:41:10 +0100

    test first name

--- greeting.js
+++ greeting.js
@@ -17,7 +17,10 @@ if (time > 18) {
 var greetingSelector = document.querySelector('.external-script-widget[data-widget-id="greeting"]');
 
 if (greetingSelector) {
-	greetingSelector.innerHTML = greeting + ' ' + we.util.ui.name(we.authMgr.getUser()) + ' Schön, dass Du da bist! ' + emoji;
+	greetingSelector.innerHTML = greeting + ' ' + we.util.ui.firstName(we.authMgr.getUser()) + ' Schön, dass Du da bist! ' + emoji;
 } else {
     console.log("This didn't work. Try again");
 }
+
+
+// Hallo,&nbsp;{{user.profile.firstName}}!<br>Schön, dass Du hier bist!
