a183fdf21da72a3dcea2ab24bae3c75f1a8e5ff3
Roman Jaeger <roman.jaeger@gmx.net>
2019-11-18 21:09:42 +0100

    stuff

--- greeting.js
+++ greeting.js
@@ -17,15 +17,15 @@ if (time > 18) {
 var greetingSelector = document.querySelector('.external-script-widget[data-widget-id="greeting"]');
 var firstName = we.authMgr.getUser().firstName;
 var profileLink = "/profile/" + we.authMgr.getUser().id
-if (greetingSelector) {
-    greetingSelector.innerHTML = greeting + ',&nbsp;' + firstName + '!<br> Schön, dass Du da bist! ' + emoji;
-} else {
-    console.log("This didn't work. Try again");
-}
-
 var a = document.createElement('a');
 var linkText = document.createTextNode("my title text");
 a.appendChild(linkText);
 a.title = "my title text";
 a.href = profileLink; 
 document.greetingSelector.appendChild(a);
+
+if (greetingSelector) {
+    greetingSelector.innerHTML = greeting + ',&nbsp;' + firstName + '!<br> Schön, dass Du da bist! ' + emoji;
+} else {
+    console.log("This didn't work. Try again");
+}
