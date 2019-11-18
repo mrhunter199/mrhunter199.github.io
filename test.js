48d79b246001aaf4361b3584137cacb3997d7636
Roman Jaeger <roman.jaeger@gmx.net>
2019-11-18 20:07:03 +0100

    update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log("Test");
+
+var myText = document.querySelector('.external-script-widget[data-widget-id="myText"]');
+
+if (myText) {
+	myText.innerHTML = 'Woohoo das hat funktioniert! ' + we.util.ui.name(we.authMgr.getUser()) + ' du bist ein Genie :D';
+} else {
+    console.log("This didn't work. Try again");
+}
