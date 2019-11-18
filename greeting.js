95e12c948b2b27126d7421df6ab94545bf31a919
Roman Jaeger <roman.jaeger@gmx.net>
2019-11-18 20:51:24 +0100

    change message

--- greeting.js
+++ greeting.js
@@ -17,7 +17,7 @@ if (time > 18) {
 var greetingSelector = document.querySelector('.external-script-widget[data-widget-id="greeting"]');
 
 if (greetingSelector) {
-    greetingSelector.innerHTML = greeting + ' ' + we.authMgr.getUser().firstName + ' Schön, dass Du da bist! ' + emoji;
+    greetingSelector.innerHTML = greeting + ',&nbsp;' + we.authMgr.getUser().firstName + '!<br> Schön, dass Du da bist! ' + emoji;
 } else {
     console.log("This didn't work. Try again");
 }
