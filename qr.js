b1126ce144d7717513e6861f5f2cb18daa95f0f8
Roman Jaeger <roman.jaeger@gmx.net>
2019-11-30 17:50:29 +0100

    add color

--- qr.js
+++ qr.js
@@ -1,5 +1,6 @@
 var charsetSource = "&charset-source=UTF-8";
 var charsetTarget = "&charset-target=UTF-8";
+var color = "&color-90-40-115"
 var firstName = encodeURIComponent(we.authMgr.getUser().firstName);
 var lastName = encodeURIComponent(we.authMgr.getUser().lastName);
 var phone = encodeURIComponent(we.authMgr.getUser().phoneNumber);
@@ -8,7 +9,7 @@ var company = encodeURIComponent(we.authMgr.getUser().profile.firma);
 var email = encodeURIComponent(we.authMgr.getUser().profile.publicEmailAddress.toLowerCase());
 var size = "&size=220x220&margin=0";
 
-var url = "https://api.qrserver.com/v1/create-qr-code/?data=BEGIN%3AVCARD%0AVERSION%3A2.1%0AFN%3A" + firstName + "+" + lastName + "%0AN%3A" + lastName + "%3B" + firstName + "%0ATEL%3BWORK%3BVOICE%3A" + phone + "%0AEMAIL%3BWORK%3BINTERNET%3A" + email + "%0AADR%3BWORK%3A%3B%3B" + company +"%0AEND%3AVCARD%0A" + size; 
+var url = "https://api.qrserver.com/v1/create-qr-code/?data=BEGIN%3AVCARD%0AVERSION%3A2.1%0AFN%3A" + firstName + "+" + lastName + "%0AN%3A" + lastName + "%3B" + firstName + "%0ATEL%3BWORK%3BVOICE%3A" + phone + "%0AEMAIL%3BWORK%3BINTERNET%3A" + email + "%0AADR%3BWORK%3A%3B%3B" + company +"%0AEND%3AVCARD%0A" + size + color; 
 
 var qrSelector = document.querySelector('.external-script-widget[data-widget-id="qrcode"]');
 
