d0f8708589fde9f1527ac6093c9f3f8be54e3fa4
Roman EBSurface <roman.jaeger@gmail.com>
2019-12-09 15:40:14 +0100

    delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log("Test");
-
-var myText = document.querySelector('.external-script-widget[data-widget-id="myText"]');
-
-if (myText) {
-	myText.innerHTML = 'Woohoo das hat funktioniert! ' + we.util.ui.name(we.authMgr.getUser()) + ' du bist ein Genie :D';
-} else {
-    console.log("This didn't work. Try again");
-}
